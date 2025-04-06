5a350824f4acf78f05b5d6c36b77a277347167e6
anasol-mo <sol.murillo@est.iudigital.edu.co>
2025-04-06 13:25:01 -0500

    Corrijo conflicto en index.js

--- src/index.js
+++ src/index.js
@@ -14,19 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas (las definiremos después)
-<<<<<<< HEAD
-app.use("/api/generos", require("./routes/genero.routes"));
-app.use("/api/directores", require("./routes/director.routes"));
-app.use("/api/productoras", require("./routes/productora.routes"));
-app.use("/api/tipos", require("./routes/tipo.routes"));
-app.use("/api/media", require("./routes/media.routes"));
-=======
+
 app.use("/api/generos", require("./routes/Genero.routes"));
 app.use("/api/directores", require("./routes/Director.routes"));
 app.use("/api/productoras", require("./routes/Productora.routes"));
 app.use("/api/tipos", require("./routes/Tipo.routes"));
 app.use("/api/media", require("./routes/Media.routes"));
->>>>>>> 53d6225 (Primer commit - Proyecto API Películas)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
